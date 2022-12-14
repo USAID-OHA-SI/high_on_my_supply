0b5c05c2a186def922a1b4168b4c31b8dfb88b60
Alexander Richardson <arichardson@credence-llc.com>
2022-12-14 16:43:13 -0500

    drc procured vs delivered

---
+++ Scripts/drc_proc_v_delivered.R
@@ -0,0 +1,78 @@
+#### Title
+# PURPOSE: DRC Procured vs. Delivered
+# AUTHOR: alerichardson | sch
+# LICENSE: MIT
+# DATE: 2022-12-14
+# NOTES: 
+
+#### LOCALS & SETUP ============================================================================
+
+
+# Libraries
+require(tidyverse)
+require(gagglr)
+require(here)
+library(googledrive)
+require(lubridate)
+
+# Set paths  
+data <- here("Data")
+dataout <- here("Dataout")
+  
+
+#### LOAD DATA ============================================================================ 
+
+#  performance dataset
+df_arv_drc <- perf_raw %>%
+  janitor::clean_names() %>%
+  filter(country == "Congo DRC",
+         str_detect(item_tracer_category, "ARV"),
+         order_type %in% c("Purchase Order", "Distribution Order"),
+         #d365_funding_source_detail %in% c("PEPFAR-COP-USAID","PEPFAR-Condom Fund"),
+         d365_health_element == "HIV/AIDS"
+         ) %>% 
+  select(item_tracer_category,
+         item_id,
+         product_name,
+         list_price,
+         ordered_quantity,
+         line_total,
+         shipped_quantity,
+         delivery_value,
+         delivery_progress,
+         latest_actual_delivery_date,
+         po_released_for_fulfillment_date,
+         order_type) %>%
+  filter(order_type != "Replenishment Order")
+
+drc_procured = df_arv_drc %>%
+  mutate(month = floor_date(po_released_for_fulfillment_date, unit = "month")) %>%
+  group_by(month, product_name) %>%
+  summarize(ordered_quantity = sum(ordered_quantity, na.rm = T),
+            line_total = sum(line_total, na.rm = T)) %>%
+  filter(month >= as_datetime("2019-01-01")) %>%
+  rename("Ordered Costs" = line_total,
+         "Ordered Quantity" = ordered_quantity) %>%
+  pivot_longer(cols = c("Ordered Quantity", "Ordered Costs"), names_to = "Category", values_to = "Value") %>%
+  pivot_wider(id_cols = c("product_name", "Category"), names_from = "month", values_from = "Value")
+  
+drc_procured[is.na(drc_procured)]<-0
+
+write_csv(drc_procured, here("Dataout", "drc_procured.csv"))
+
+drc_delivered = df_arv_drc %>%
+  filter(delivery_progress == 2) %>%
+  mutate(month = floor_date(latest_actual_delivery_date, unit = "month")) %>%
+  group_by(month, product_name) %>%
+  summarize(shipped_quantity = sum(shipped_quantity, na.rm = T),
+            delivery_value = sum(delivery_value, na.rm = T)) %>%
+  filter(month >= as_datetime("2019-01-01") &
+           month < as_datetime("2022-12-14")) %>%
+  rename("Delivered Value" = delivery_value,
+         "Delivered Quantity" = shipped_quantity) %>%
+  pivot_longer(cols = c("Delivered Quantity", "Delivered Value"), names_to = "Category", values_to = "Value") %>%
+  pivot_wider(id_cols = c("product_name", "Category"), names_from = "month", values_from = "Value")
+
+drc_delivered[is.na(drc_delivered)]<-0
+
+write_csv(drc_delivered, here("Dataout", "drc_delivered.csv"))
--- Scripts/pvp_table.R
+++ Scripts/pvp_table.R
@@ -10,77 +10,94 @@
 require(xlsx)
 
 #### Tabular Presentation ============================================================================
+filepath = here("Dataout", "plannedVsProcured.xlsx")
 
-# Initialize Workbook
-wb = xlsx::createWorkbook()
-
-# Basic Setup
-titleStyle = CellStyle(wb) +
-  Font(wb, isBold = T)
-columnStyle = CellStyle(wb) +
-  Font(wb, isBold = T) + 
-  Fill(backgroundColor = "#ABD6DC")
-dataStyle = CellStyle(wb) +
-  Alignment(h="ALIGN_RIGHT")
-
-# Loop though countries
-for(country_sel in unique(df_performance$country)){
+pvp_table = function(df_pvp, categories, filepath) {
+  # Initialize Workbook
+  wb = xlsx::createWorkbook()
   
-  # Limit to Country
-  df_local = df_pvp %>%
-    filter(country == country_sel,
-           !is.na(line_total) | !is.na(total_cost)) %>%
-    mutate(product_name = case_when(
-      is.na(product_name) ~ match_name,
-      !is.na(product_name) ~ product_name
-    )) %>% 
-    mutate(quantity_difference = item_quantity-ordered_quantity,
-           cost_difference = total_cost-line_total) %>%
-    select(product_name, ordered_quantity, item_quantity, quantity_difference, line_total, total_cost, cost_difference, category)
+  # Basic Setup
+  titleStyle = CellStyle(wb) +
+    Font(wb, isBold = T)
+  columnStyle = CellStyle(wb) +
+    Font(wb, isBold = T) +
+    Fill(backgroundColor = "#ABD6DC")
+  dataStyle = CellStyle(wb) +
+    Alignment(h = "ALIGN_RIGHT")
   
-  # Tab Setup
-  rowCounter = 1
-  sheet = xlsx::createSheet(wb, sheetName = country_sel)
-  rows <- createRow(sheet, 1:100)
-  cells <- createCell(rows, colIndex = 1:12)
-  
-  # Loop through Categories
-  for(category_sel in categories){
+  # Loop though countries
+  for (country_sel in unique(df_pvp$country)) {
+    # Limit to Country
+    df_local = df_pvp %>%
+      filter(country == country_sel,!is.na(line_total) |
+               !is.na(total_cost)) %>%
+      mutate(product_name = case_when(
+        is.na(product_name) ~ match_name,!is.na(product_name) ~ product_name
+      )) %>%
+      mutate(
+        quantity_difference = item_quantity - ordered_quantity,
+        cost_difference = total_cost - line_total
+      ) %>%
+      select(
+        product_name,
+        ordered_quantity,
+        item_quantity,
+        quantity_difference,
+        line_total,
+        total_cost,
+        cost_difference,
+        category
+      )
     
-    # Header
-    setCellValue(cells[[rowCounter,1]], category_sel)
-    setCellStyle(cells[[rowCounter,1]], titleStyle)
-    rowCounter= rowCounter + 1
+    # Tab Setup
+    rowCounter = 1
+    sheet = xlsx::createSheet(wb, sheetName = country_sel)
+    rows <- createRow(sheet, 1:100)
+    cells <- createCell(rows, colIndex = 1:12)
     
-    # Columns
-    setCellValue(cells[[rowCounter,1]], "Product Name")
-    setCellValue(cells[[rowCounter,2]], "Planned Quantity")
-    setCellValue(cells[[rowCounter,3]], "Quantity Difference")
-    setCellValue(cells[[rowCounter,4]], "Procured Quantity")
-    setCellValue(cells[[rowCounter,5]], "Procured Cost")
-    setCellValue(cells[[rowCounter,6]], "Planned Cost")
-    setCellValue(cells[[rowCounter,7]], "Cost Difference")
-    setCellStyle(cells[[rowCounter,1]], columnStyle)
-    setCellStyle(cells[[rowCounter,2]], columnStyle)
-    setCellStyle(cells[[rowCounter,3]], columnStyle)
-    setCellStyle(cells[[rowCounter,4]], columnStyle)
-    setCellStyle(cells[[rowCounter,5]], columnStyle)
-    setCellStyle(cells[[rowCounter,6]], columnStyle)
-    setCellStyle(cells[[rowCounter,7]], columnStyle)
-    rowCounter = rowCounter + 1
-    
-    # Data
-    df_cat = df_local %>%
-      filter(category == category_sel) %>%
-      select(-category)
-    addDataFrame(df_cat, sheet, col.names = F, row.names = F, startRow = rowCounter, colStyle = dataStyle)
-    
-    rowCounter = rowCounter + length(df_cat$product_name) + 2
-    
+    # Loop through Categories
+    for (category_sel in categories) {
+      # Header
+      setCellValue(cells[[rowCounter, 1]], category_sel)
+      setCellStyle(cells[[rowCounter, 1]], titleStyle)
+      rowCounter = rowCounter + 1
+      
+      # Columns
+      setCellValue(cells[[rowCounter, 1]], "Product Name")
+      setCellValue(cells[[rowCounter, 2]], "Planned Quantity")
+      setCellValue(cells[[rowCounter, 3]], "Quantity Difference")
+      setCellValue(cells[[rowCounter, 4]], "Procured Quantity")
+      setCellValue(cells[[rowCounter, 5]], "Procured Cost")
+      setCellValue(cells[[rowCounter, 6]], "Planned Cost")
+      setCellValue(cells[[rowCounter, 7]], "Cost Difference")
+      setCellStyle(cells[[rowCounter, 1]], columnStyle)
+      setCellStyle(cells[[rowCounter, 2]], columnStyle)
+      setCellStyle(cells[[rowCounter, 3]], columnStyle)
+      setCellStyle(cells[[rowCounter, 4]], columnStyle)
+      setCellStyle(cells[[rowCounter, 5]], columnStyle)
+      setCellStyle(cells[[rowCounter, 6]], columnStyle)
+      setCellStyle(cells[[rowCounter, 7]], columnStyle)
+      rowCounter = rowCounter + 1
+      
+      # Data
+      df_cat = df_local %>%
+        filter(category == category_sel) %>%
+        select(-category)
+      addDataFrame(
+        df_cat,
+        sheet,
+        col.names = F,
+        row.names = F,
+        startRow = rowCounter,
+        colStyle = dataStyle
+      )
+      
+      rowCounter = rowCounter + length(df_cat$product_name) + 2
+      
+    }
   }
+  
+  # Save
+  saveWorkbook(wb, filepath)
 }
-
-# Save
-saveWorkbook(wb, here("Dataout", "plannedVsProcured.xlsx"))
-
 
